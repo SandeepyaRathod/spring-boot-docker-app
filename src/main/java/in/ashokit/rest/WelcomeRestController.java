918209c7686ddf99f4d862ec9e6140ce10d9c409
SandeepyaRathod <sandeepa.rathod123@gmail.com>
2025-12-01 18:25:12 +0530

    Update WelcomeRestController.java

--- src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -22,6 +22,7 @@ public class WelcomeRestController {
 		logger.info("***** welcomeMsg() execution end *****");
 		String s = "hello";
 		logger.info("******************************************************************");
+		logger.info("********************* Changes Added *******************");
 		return msg;
 	}
 	
